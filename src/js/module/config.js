9f99848dafda807cd3b5913334e14a61acb45e11
haili042 <haili042@163.com>
2016-03-27 22:10:42 +0800

    路由配置先提交一次备份

--- src/js/module/config.js
+++ src/js/module/config.js
@@ -8,32 +8,38 @@ App.config(['$stateProvider', '$urlRouterProvider',
     function ($stateProvider, $urlRouterProvider) {
         'use strict';
 
-        // 默认路由
+        // 无效路由负责, $urlRouterProvider 负责配置指定url之外的url请求
         $urlRouterProvider.otherwise('/');
 
         // 路由配置
         // 注意这里采用的是ui-router这个路由，而不是ng原生的路由。ng原生的路由不能支持嵌套视图
         $stateProvider
             .state('/', {
-                url: '/index',
-                title: '首页',
-                views: {
-                    '': {
-                        templateUrl: 'app/tpl/index.html'
-                    }
-
-                }
+                url: '/',
+                templateUrl: 'app/tpl/home/login.html'
             })
 
             // 类似 struts 的通配符
-            // bss: business 业务文件夹, 与 tpl 下的文件夹名一一对应
-            .state('/:bss', {
+            // :bss 即 business, 业务文件夹, 与 tpl 下的文件夹名一一对应
+            // :page 与 bss 内的文件名一一对应
+            // 先匹配 home, 然后匹配 url, 即 home/:bss/page
+            .state(':bss', {
                 // page 与 bss 内的文件名一一对应
-                url: '/:bss/:page',
-                title: '用户管理',
-                templateUrl: function ($routeParams) {
-                    // 添加时间戳, 防止缓存
-                    return 'app/tpl/' + $routeParams.bss + '/' + $routeParams.page + '.html?_=' + new Date().getTime();
+                url: '/:page',
+                views: {
+                    '': {
+                        templateUrl: 'app/tpl/home/layout.html' // 默认值
+                    },
+                    'content@:bss/:page': {
+                        templateUrl: function ($routeParams) {
+                            var bss = $routeParams.bss || 'home';
+                            var page = $routeParams.page || 'home' ;
+                            console.log(bss);
+                            console.log(page);
+                            // 添加时间戳, 防止缓存
+                            return 'app/tpl/' + bss + '/' + page + '.html?_=' + new Date().getTime();
+                        }
+                    }
                 }
             })
         ;
